f18db59ebaefaeccf824cb5b2a4d8cd04ced332d
Anurag <anuragagrawal4998@gmail.com>
2020-07-30 18:33:53 +0530

    animate

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Col, Form, FormGroup, Label, Input, Button } from 'reactstrap';
-import Fade from 'react-reveal/Fade'; // Importing Zoom effect
 
 const Contact = () =>
 
@@ -10,40 +9,38 @@ const Contact = () =>
             <div className="mb-5 text-center contact-section-intro p-1">Get in touch if you have any questions or queries</div>
 
             <div className="container">
-                <Fade left>
-                    <div id="contact-form" className="col-lg-9 mx-lg-auto formcontainer p-3">
-                        <h3 className="mb-4 text-center contact-section-title">Ask a Question</h3>
+                <div id="contact-form" className="col-lg-9 mx-lg-auto formcontainer p-3">
+                    <h3 className="mb-4 text-center contact-section-title">Ask a Question</h3>
 
-                        <Form>
+                    <Form>
 
-                            <FormGroup row className="contactusform">
-                                <Col sm={12} md={6}>
-                                    <input type="text" className="form-control mb-2" placeholder="Name" minlength="2" required="" />
-                                </Col>
-                                <Col sm={12} md={6}>
+                        <FormGroup row className="contactusform">
+                            <Col sm={12} md={6}>
+                                <input type="text" className="form-control mb-2" placeholder="Name" minlength="2" required="" />
+                            </Col>
+                            <Col sm={12} md={6}>
 
-                                    <input type="email" className="form-control" placeholder="Email " color="muted" required="" />
-                                </Col>
+                                <input type="email" className="form-control" placeholder="Email " color="muted" required="" />
+                            </Col>
 
-                            </FormGroup>
-                            <FormGroup row>
-                                <Col sm={12}>
-                                    <Label for="exampleText"></Label>
-                                    <Input type="textarea" name="text" id="exampleText" rows="10" placeholder="Enter your message" className="form-control align-items-center" autoComplete="off" />
-                                </Col>
-                            </FormGroup>
-
-
-                        </Form>
+                        </FormGroup>
                         <FormGroup row>
                             <Col sm={12}>
-                                <Button id="ctl00_MainContentHolder_butSubmit" color="primary" className="btn btn-block btn-primary py-2" >
-                                    Submit</Button>
+                                <Label for="exampleText"></Label>
+                                <Input type="textarea" name="text" id="exampleText" rows="10" placeholder="Enter your message" className="form-control align-items-center" autoComplete="off" />
                             </Col>
                         </FormGroup>
 
-                    </div>
-                </Fade>
+
+                    </Form>
+                    <FormGroup row>
+                        <Col sm={12}>
+                            <Button id="ctl00_MainContentHolder_butSubmit" color="primary" className="btn btn-block btn-primary py-2" >
+                                Submit</Button>
+                        </Col>
+                    </FormGroup>
+
+                </div>
             </div>
         </section>
     )
--- src/components/pricing.js
+++ src/components/pricing.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { FiArrowRightCircle } from "react-icons/fi";
 import { Button } from 'reactstrap';
+import Fade from 'react-reveal/Fade';
 
+import Pulse from 'react-reveal/Pulse';
 
 
 const Pricing = () => {
@@ -19,104 +21,105 @@ const Pricing = () => {
             <div className="container ">
                 <h3 className="mb-2 text-center font-weight-bold price-section-title">Pick the best plan for you</h3>
                 <div className="mb-5 text-center section-intro">Got you covered for all your needs</div>
-
-                <div className="row">
-                    <div className="col-12 col-md-6 col-lg-2 mr-auto plan-container">
-                        <div className="Pricing-title">
-                            <h4>Free</h4>
-                            <p className="plan-price"> 0$ <span>/ month </span> </p>
-                            <Button href="#">SIGN UP</Button>
+                <Fade left duration={1500}>
+                    <div className="row">
+                        <div className="col-12 col-md-6 col-lg-2 mr-auto plan-container">
+                            <div className="Pricing-title">
+                                <h4>Free</h4>
+                                <p className="plan-price"> 0$ <span>/ month </span> </p>
+                                <Button href="#">SIGN UP</Button>
+                            </div>
+                            <div className="Pricing-content">
+                                <ul className="list-unstyled">
+                                    <li>10,000 calls/month </li>
+                                    <li>Realtime weather</li>
+                                    <li>Weather History (Last 7 days only)</li>
+                                    <li>HTTP included</li>
+                                    <li>Search API</li>
+                                </ul>
+                            </div>
                         </div>
-                        <div className="Pricing-content">
-                            <ul className="list-unstyled">
-                                <li>10,000 calls/month </li>
-                                <li>Realtime weather</li>
-                                <li>Weather History (Last 7 days only)</li>
-                                <li>HTTP included</li>
-                                <li>Search API</li>
-                            </ul>
+                        <div className="col-12 col-md-6 col-lg-2 mr-auto plan-container">
+                            <div className="Pricing-title">
+                                <h4>Developer</h4>
+                                <p className="plan-price">29$ <span>/ month </span> </p>
+                                <Button href="#">SIGN UP</Button>
+
+                            </div>
+                            <div className="Pricing-content">
+                                <ul className="list-unstyled">
+                                    <li>50,000 calls/month </li>
+                                    <li>Realtime weather</li>
+                                    <li>Weather History (Last 10 days only)</li>
+                                    <li>HTTPS included</li>
+                                    <li>Search API</li>
+                                </ul>
+
+                            </div>
+                        </div>
+                        <div className={scroll ? "col-12 col-md-6 col-lg-2 mr-auto plan-container" : "col-12 col-md-6 col-lg-2 mr-auto plan-container most-popular"}>
+                            <div className="Pricing-title">
+                                <h4>Professional</h4>
+                                <p className="plan-price">69$ <span>/ month </span> </p>
+                                <Pulse delay={2000}> <Button href="#">SIGN UP</Button></Pulse>
+
+                            </div>
+                            <div className="Pricing-content">
+                                <ul className="list-unstyled">
+                                    <li>100,000 calls/month </li>
+                                    <li>Realtime weather</li>
+                                    <li>Weather History (Last 21 days only)</li>
+                                    <li>HTTPS included</li>
+                                    <li>Search API</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-12 col-md-6 col-lg-2 mr-auto plan-container">
+                            <div className="Pricing-title">
+                                <h4>Business</h4>
+                                <p className="plan-price">99$ <span>/ month </span> </p>
+                                <Button href="#">SIGN UP</Button>
+
+                            </div>
+                            <div className="Pricing-content">
+                                <ul className="list-unstyled">
+                                    <li>1,000,000 calls/month </li>
+                                    <li>Realtime weather</li>
+                                    <li>Weather History (1st Jan, 2015 onwards)</li>
+                                    <li>HTTPS included</li>
+                                    <li>Search API</li>
+                                </ul>
+
+                            </div>
+                        </div>
+                        <div className="col-12 col-md-6 col-lg-2 mr-auto plan-container">
+                            <div className="Pricing-title">
+                                <h4>Enterprise</h4>
+                                <p className="plan-price">  <span> Custom Prices</span> </p>
+                                <Button href="#">Contact Us</Button>
+
+                            </div>
+                            <div className="Pricing-content">
+                                <ul className="list-unstyled">
+
+                                    <li>High Volume calls/month </li>
+                                    <li>Realtime weather</li>
+                                    <li>Weather History (1st Jan, 2015 onwards)</li>
+                                    <li>HTTPS included</li>
+                                    <li>Search API</li>
+                                    <li>Priority Support</li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
-                    <div className="col-12 col-md-6 col-lg-2 mr-auto plan-container">
-                        <div className="Pricing-title">
-                            <h4>Developer</h4>
-                            <p className="plan-price">29$ <span>/ month </span> </p>
-                            <Button href="#">SIGN UP</Button>
-
-                        </div>
-                        <div className="Pricing-content">
-                            <ul className="list-unstyled">
-                                <li>50,000 calls/month </li>
-                                <li>Realtime weather</li>
-                                <li>Weather History (Last 10 days only)</li>
-                                <li>HTTPS included</li>
-                                <li>Search API</li>
-                            </ul>
-
-                        </div>
+                    <div className="more-features">
+                        <h6>View More Details </h6>
+                        <FiArrowRightCircle className="more-features-icons" />
                     </div>
-                    <div className={scroll ? "col-12 col-md-6 col-lg-2 mr-auto plan-container" : "col-12 col-md-6 col-lg-2 mr-auto plan-container most-popular"}>
-                        <div className="Pricing-title">
-                            <h4>Professional</h4>
-                            <p className="plan-price">69$ <span>/ month </span> </p>
-                            <Button href="#">SIGN UP</Button>
+                </Fade>
 
-                        </div>
-                        <div className="Pricing-content">
-                            <ul className="list-unstyled">
-                                <li>100,000 calls/month </li>
-                                <li>Realtime weather</li>
-                                <li>Weather History (Last 21 days only)</li>
-                                <li>HTTPS included</li>
-                                <li>Search API</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="col-12 col-md-6 col-lg-2 mr-auto plan-container">
-                        <div className="Pricing-title">
-                            <h4>Business</h4>
-                            <p className="plan-price">99$ <span>/ month </span> </p>
-                            <Button href="#">SIGN UP</Button>
-
-                        </div>
-                        <div className="Pricing-content">
-                            <ul className="list-unstyled">
-                                <li>1,000,000 calls/month </li>
-                                <li>Realtime weather</li>
-                                <li>Weather History (1st Jan, 2015 onwards)</li>
-                                <li>HTTPS included</li>
-                                <li>Search API</li>
-                            </ul>
-
-                        </div>
-                    </div>
-                    <div className="col-12 col-md-6 col-lg-2 mr-auto plan-container">
-                        <div className="Pricing-title">
-                            <h4>Enterprise</h4>
-                            <p className="plan-price">  <span> Custom Prices</span> </p>
-                            <Button href="#">Contact Us</Button>
-
-                        </div>
-                        <div className="Pricing-content">
-                            <ul className="list-unstyled">
-
-                                <li>High Volume calls/month </li>
-                                <li>Realtime weather</li>
-                                <li>Weather History (1st Jan, 2015 onwards)</li>
-                                <li>HTTPS included</li>
-                                <li>Search API</li>
-                                <li>Priority Support</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div className="more-features">
-                    <h6>View More Details </h6>
-                    <FiArrowRightCircle className="more-features-icons" />
-                </div>
             </div>
-
-        </section>
+        </section >
     )
 }
 
